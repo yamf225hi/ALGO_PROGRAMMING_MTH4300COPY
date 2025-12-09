81fb4d0ac54eb01089cb451a0bf8b8f1b96218d2
fardeenmac <fardeen@Munas-MacBook-Air.local>
2025-12-08 19:16:00 -0500

    all

--- homeworks/hw4/library.cpp
+++ homeworks/hw4/library.cpp
@@ -1,28 +1,12 @@
 #include "library.h"
-#include "Book2.cpp"
-
 #include "Book2.h"
 #include <string>
 
 
-//constructor defintin: 
-Book::Book(const std::string& bookTitle, const std::string& bookAuthor, int publishedYear, double bookPrice) 
-    :title(bookTitle), author(bookAuthor), yearPublished(publishedYear),price(bookPrice)
-    {
-            //construcor body. empty since all work done in the initializer list. 
-    }
-
-void Book::applyDiscount(double discountPERCENTAGE)
-    {
-        price -= price * (discountPERCENTAGE / 100.0);
-    }
-
-
-std::string Book::getbookINFO() const
-{
-    std::string bookinfo = 
-        "Title" + title + "Author" + author + "Year Published" + std::to_string(yearPublished) + "price" + std::to_string(price);
-
-    return bookinfo;
-
+Library::Library(): books(size(0))
+:size_t(0) {
+    
 }
+
+
+
