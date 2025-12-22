863defd02700b255e1d66b5403197d5d31fa1e0d
fardeenmac <fardeen@Munas-MacBook-Air.local>
2025-12-21 21:08:18 -0500

    coddy

---
+++ my practice/coddytech.cpp
@@ -0,0 +1,18 @@
+#include <string>
+using namespace std;
+
+class Athlete {
+public:
+
+    string name;
+    int age;
+    float salary;
+
+    Athlete()
+    {
+        this->name = "";
+        this->age = 0;
+        this->salary= 0;
+    }
+
+};
